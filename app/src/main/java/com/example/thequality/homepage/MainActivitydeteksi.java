9ad4d46d78fc9cfae5e0e19894d0baf83de80f0d
Kiyaaa <hafizazakia1705@gmail.com>
2026-05-12 09:43:31 +0700

    initial commit

--- app/src/main/java/com/example/thequality/homepage/MainActivitydeteksi.java
+++ app/src/main/java/com/example/thequality/homepage/MainActivitydeteksi.java
@@ -45,7 +45,7 @@ public class MainActivitydeteksi extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_main2);
 
 
         previewView = findViewById(R.id.previewView);
